5d80f07d1cc8aac2f701eca43460923df1c41fc9
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-08-31 09:40:48 -0700

    Init commit

---
+++ app/app.js
@@ -0,0 +1,5 @@
+'use strict';
+var app = angular.module('sortjs', []);
+var controllers = angular.module('sortjs.controllers', []);
+var directives = angular.module('sortjs.directives', []);
+var services = angular.module('sortjs.services', []);

bab8d30879f21d002f5983bca0ac4f16120e6561
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-09-01 10:52:34 -0700

    incorporates Notifier.js

--- app/services.js
+++ app/services.js
@@ -1,1 +1,36 @@
-'use strict';
+'use strict';services.factory('notifier', [function() {
+    var factory = {};
+    factory.success = function(message, title) {
+        if(title) {
+            Notifier.success(message, title);
+        } else {
+            Notifier.success(message);
+        }
+    };
+
+    factory.info = function(message, title) {
+        if(title) {
+            Notifier.info(message, title);
+        } else {
+            Notifier.info(message);
+        }
+    };
+
+    factory.warning = function(message, title) {
+        if(title) {
+            Notifier.warning(message, title);
+        } else {
+            Notifier.warning(message);
+        }
+    };
+
+    factory.error = function(message, title) {
+        if(title) {
+            Notifier.error(message, title);
+        } else {
+            Notifier.error(message);
+        }
+    };
+
+    return factory;
+}]);

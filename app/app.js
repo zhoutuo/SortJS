e1747b1a6ad2b9ad79aa304a5e818c47fb31611b
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-09-01 11:02:01 -0700

    Add logic to left panel.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 'use strict';
-var app = angular.module('sortjs', []);
+var app = angular.module('sortjs', ['sortjs.controllers', 'sortjs.directives', 'sortjs.services']);
 var controllers = angular.module('sortjs.controllers', []);
 var directives = angular.module('sortjs.directives', []);
 var services = angular.module('sortjs.services', []);
--- app/controllers.js
+++ app/controllers.js
@@ -1,1 +1,54 @@
 'use strict';
+controllers.controller('baseCtrl', ['$scope', 'notifier', 'sorters', function($scope, notifier, sorters) {
+    //variables init
+    $scope.algos = sorters.getSorters();
+    $scope.random_len = "";
+    $scope.input_array = "";
+    $scope.selected_sorter = undefined;
+    $scope.randomGenerate = function() {
+        if($scope.random_len === "" || isNaN(Number($scope.random_len))) {
+            // default value of 10
+            $scope.random_len = "10";
+        }
+        var len = parseInt($scope.random_len, 10);
+
+        var randomInt = function(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+
+        var output = [];
+        for(var i = 0; i < len; ++i) {
+            output.push(randomInt(-100000, 100000));
+        }
+        // populate
+        $scope.input_array = output.join(", ");
+    };
+
+
+    $scope.run = function() {
+        if($scope.input_array === "") {
+            notifier.error("please input numbers to run");
+            return;
+        }
+        if($scope.selected_sorter === undefined) {
+            notifier.error("please select a sorter to run");
+            return;
+        }
+        //parse the input array
+        var nums = $scope.input_array.split(',');
+        for(var i = 0; i < nums.length; ++i) {
+            //convert all strings to numbers
+            nums[i] = Number(nums[i]);
+            //if Nan exists
+            if(isNaN(nums[i])) {
+                notifier.error("detected a Nan", "Parsing error")
+                return;
+            }
+        }
+        //fire the event
+        $scope.$broadcast('run', {
+            sorter: $scope.selected_sorter,
+            numbers: nums
+        })
+    };
+}]);
